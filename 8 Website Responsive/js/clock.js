0cb059ea70e1eb82c5aff09d57ad2c6f1c6c882f
JohannGio <105080354+JohannGio@users.noreply.github.com>
2022-07-06 14:25:43 -0400

    HTML CSS JS only
    We built this amazing and totaly responsive website dashboard for admin using HTML,CSSand JS only.
    no frameworks nor bootstrap.

---
+++ 8 Website Responsive/js/clock.js
@@ -0,0 +1,15 @@
+/*======== CLOCK TIME========== */
+function displayTime(){
+    let dateTime = new Date();
+
+    let hrs = dateTime.getHours();
+    let min = dateTime.getMinutes();
+    let sec = dateTime.getSeconds();
+    let session = document.getElementById('session');
+
+    document.getElementById('hours').innerHTML = hrs;
+    document.getElementById('minutes').innerHTML = min;
+    document.getElementById('seconds').innerHTML = sec;
+}
+
+setInterval(displayTime, 10);
---
+++ 8 Website Responsive/js/orders.js
@@ -0,0 +1,32 @@
+const orders = [
+{
+    productName:"Ruko Fu Pro Drone",
+    productNumber:"49347",
+    paymentStatus:"due",
+    shipping:"Pending"
+},
+{
+    productName:"Camera II Pro Drone",
+    productNumber:"98745",
+    paymentStatus:"Paid",
+    shipping:"Delivered"
+},
+{
+    productName:"Sistem MDS",
+    productNumber:"42817",
+    paymentStatus:"Paid",
+    shipping:"Delivered"
+},
+{
+    productName:"Ultra Camera",
+    productNumber:"84265",
+    paymentStatus:"Paid",
+    shipping:"Delivered"
+},
+{
+    productName:"Drone XS14",
+    productNumber:"94182",
+    paymentStatus:"Due",
+    shipping:"Pending"
+},
+]
